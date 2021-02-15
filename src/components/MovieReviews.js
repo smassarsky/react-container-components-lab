93b3b8f8e34ab8db9bd8339c7c0234b00cdb8cfb
smassarsky <seth.massarsky@gmail.com>
2021-02-15 15:54:49 -0500

    commit tree ee516f7714d0b82094f8476d50dda3be9fc55cd6

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => {
+  return <div className="review-list">
+    {renderReviews(props.reviews)}
+  </div>
+}
+
+const renderReviews = reviews => {
+  return reviews.map(review => {
+    return <div key={review['display_title']} className="review">
+      {review.multimedia ? <img src={review.multimedia.src} alt={review['display_title']}/> : "" }
+      <h3>{review.display_title}</h3>
+      <p>Opening Date: {review['opening_date']}</p>
+      <p>Rating: {review['mpaa_rating']}</p>
+      <p>Summary: {review['summary_short']}</p>
+    </div>
+  })
+}
+
+export default MovieReviews
